4ca251d57005da3645ffaf07bfeb27b3034c4061
Lee Fox <lee.fox@sonarsource.com>
2024-06-18 15:42:14 -0500

    updated

--- src/main/java/com/example/fizzbang/FizzBangController.java
+++ src/main/java/com/example/fizzbang/FizzBangController.java
@@ -34,6 +34,8 @@ public class FizzBangController {
         }
 
         String userId = "LeeFox";
+	String password = "password";
+        String database = "database";
 
         List<String> result = new ArrayList<>();
         int fizzBangCount = 0; // Counter for "FizzBang"
